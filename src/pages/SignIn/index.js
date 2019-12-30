eb863eae51d33ea8681d3900230316ec615966d0
danilodesousa <danilod2.sousa@gmail.com>
2019-12-30 13:10:42 -0300

    Gobarber web commit inicial

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import * as actions from '../../store/modules/auth/actions';
+
+import logo from '../../assets/logo.svg';
+
+export default function SignIn() {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Insira um e-mail válido')
+      .required('O e-mail é obrigatório'),
+    password: Yup.string()
+      .min(6, 'Mínimo de 6 caracteres')
+      .required('A senha é obrigatória')
+  });
+
+  function handleSubmit({ email, password }) {
+    dispatch(actions.signInRequest(email, password));
+    console.tron.log({ dispatchLog: email, password });
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
+}
